f0ad6849fb254e8686ce0acf20d1d4e270af3c37
Empressfavs <favouradaavwopiabro@gmail.com>
2024-03-11 09:02:36 +0100

    a class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models.engine.file_storage import storage
 
 
 """
@@ -29,6 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def update_time(self):
         """Updates the update_at attribute with the current datetime"""
@@ -36,7 +38,9 @@ class BaseModel:
 
     def save(self):
         """updates the updated_at attribute with the current datetime"""
-        self.update_time()
+        self.updated_time() = datetime.now()
+        storage.save()
+
 
     def to_dict(self):
         """converts the instance attributes to a dictionary.
