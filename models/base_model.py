5760968d1e9d4b27b32bb5bb98bf60787b9424b8
Empressfavs <favouradaavwopiabro@gmail.com>
2024-03-12 20:20:01 +0100

    re-create an instance with dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,24 @@ class BaseModel:
     """a class BaseModel that defines all common attributes/methods
     for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Constructor method for BaseModel class"
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:   
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Method to update the updated_at attribute with the current datetime"""
